d68337da2ab06dcf97eee8b1eed4789ce3dfa483
tripp3labs <64626824+joshnel2@users.noreply.github.com>
2025-07-14 12:41:09 -0400

    Create meidationaiapp.swift

---
+++ meidationaiapp.swift
@@ -0,0 +1,23 @@
+//
+//  MediationAIApp.swift
+//  meidationaiapp
+//
+//  Created by Linda Alster on 7/14/25.
+//
+
+
+import SwiftUI
+
+@main
+struct MediationAIApp: App {
+    @StateObject var authService = MockAuthService()
+    @StateObject var disputeService = MockDisputeService()
+    
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+                .environmentObject(authService)
+                .environmentObject(disputeService)
+        }
+    }
+}
